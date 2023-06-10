396120e938c50abcac876e9f7261f36664bef100
iodinechan <bubu2daily@gmail.com>
2023-06-10 00:19:55 -0600

    20230609 Implemented the logic: 1) Choose the game mode (1 player or 2 players) 2) Input the player names, create the player list and assign the players sequence randomly 3) Completed the winning conditions checking logic (CheckDiagonal1 and CheckDiagonal2)

--- Connect4_IreneChan/Program.cs
+++ Connect4_IreneChan/Program.cs
@@ -10,20 +10,83 @@ namespace Connect4_IreneChan
         public static int BoardCol = 7;
         private static List<Player> _playerList;
         private static int _currentTurn;
+        private static Random r;
 
-        public Connect4Controller()
+        static Connect4Controller()
         {
             _playerList= new List<Player>();
             _currentTurn = 0;
+            r = new Random();
+        }
 
-            /*
-            playerList = new List<Player>();
-            var HP = new HumanPlayer();
-            var CP = new ComputerPlayer();
+        public static void PlayConnect4()
+        {
+            ChoosingGameMode();
+            RandomPlayersSeq();
+            DisplayPlayers();
+        }
 
-            playerList.Add(HP);
-            playerList.Add(CP);
-            */
+        private static void ChoosingGameMode()
+        {
+            Console.WriteLine("Enter the number of player(s) [ 1 or 2 ]: ");
+            int noOfPlayers;
+
+            while (true)
+            {
+                noOfPlayers = int.Parse(Console.ReadLine());
+                if (noOfPlayers == 1 || noOfPlayers == 2)
+                {
+                    CreatePlayerList(noOfPlayers);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("The number of player(s) should be 1 or 2: ");
+                }
+            }
+        }
+
+        private static void CreatePlayerList(int noOfPlayers)
+        {
+            if(noOfPlayers == 2)
+            {
+                for(int i = 0; i < noOfPlayers; i++)
+                {
+                    _playerList.Add(new HumanPlayer(InputPlayer(i)));
+                }
+            }else if (noOfPlayers == 1)
+            {
+                _playerList.Add(new HumanPlayer(InputPlayer(0)));
+                _playerList.Add(new ComputerPlayer());
+            }
+        }
+
+        private static string InputPlayer(int seq)
+        {
+            if (seq == 0) Console.WriteLine("Enter the 1st player's name: ");
+            else if (seq == 1) Console.WriteLine("Enter the 2nd player's name: ");
+
+            return Console.ReadLine();
+        }
+
+        private static void RandomPlayersSeq()
+        {
+            // Randomly assign the sequence of the players
+            if(r.Next(0, 2) == 0) _playerList.Reverse();
+        }
+
+        private static void DisplayPlayers()
+        {
+            for(int i = 0; i < _playerList.Count; i++)
+            {
+                Console.WriteLine($"Player {i+1} is {_playerList[i].Name}");
+            }
+        }
+        private static void RestartGame()
+        {
+            _playerList.Clear();
+            _currentTurn = 0;
+            PlayConnect4();
         }
 
         public int NextTurn()
@@ -41,12 +104,22 @@ namespace Connect4_IreneChan
     {
         public string Name { get; set; }
 
+        public Player(string name)
+        {
+            Name = name;
+        }
+
         public abstract void Play();
     }
 
     // The Human Player Class from Player Abstract
     internal class HumanPlayer : Player
     {
+        public HumanPlayer(string name) : base(name)
+        {
+
+        }
+
         public override void Play()
         {
             
@@ -56,6 +129,11 @@ namespace Connect4_IreneChan
     // The Computer Player Class from Player Abstract
     internal class ComputerPlayer : Player
     {
+        public ComputerPlayer() : base("Computer")
+        {
+            
+        }
+
         public override void Play()
         {
 
@@ -87,12 +165,12 @@ namespace Connect4_IreneChan
             // For Testing
             GameMap = new int[6, 7] 
             {
+                { 0, 0, 1, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 0 },
                 { 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 1, 1, 1, 1, 0 }
+                { 0, 0, 0, 0, 0, 0, 0 }
             };
             
         }
@@ -110,22 +188,22 @@ namespace Connect4_IreneChan
 
         private static bool CheckVertical(int player, int lastStepX, int lastStepY)
         {
-            // The method is completed!!!
+            // This method is completed!!!
 
             // Check the vertical condition of last step
             int[,] gameMap = GameStatus.GameMap;
             int count = 1;
 
             // Check upper direction
-            for(int i = 1; i < Connect4Controller.BoardRow - lastStepY; i++)
-            {
-                if (gameMap[lastStepY + i, lastStepX] == player) count++;
-            }
-
-            // Check lower direction
             for (int i = 1; i <= lastStepY; i++)
             {
                 if (gameMap[lastStepY - i, lastStepX] == player) count++;
+            }
+
+            // Check lower direction
+            for (int i = 1; i < Connect4Controller.BoardRow - lastStepY; i++)
+            {
+                if (gameMap[lastStepY + i, lastStepX] == player) count++;
             }
 
             if (count >= 4) return true;
@@ -135,7 +213,7 @@ namespace Connect4_IreneChan
 
         private static bool CheckHorizontal(int player, int lastStepX, int lastStepY)
         {
-            // The method is completed!!!
+            // This method is completed!!!
 
             // Check the horizontal condition of last step
             int[,] gameMap = GameStatus.GameMap;
@@ -160,14 +238,75 @@ namespace Connect4_IreneChan
 
         private static bool CheckDiagonal1(int player, int lastStepX, int lastStepY)
         {
-            // Check the first diagonal condition of last step
+            // This method is completed!!!
+
+            // Check the first diagonal (from left bottom to right up) condition of last step
+            int[,] gameMap = GameStatus.GameMap;
+            int count = 1;
+            int checkStepRightUp;
+            int checkStepLeftBottom;
+
+            // Check right up direction
+            if (Connect4Controller.BoardCol - lastStepX < lastStepY) 
+                checkStepRightUp = Connect4Controller.BoardCol - lastStepX;
+            else 
+                checkStepRightUp = lastStepY;
+
+            for (int i = 1; i < checkStepRightUp; i++)
+            {
+                if (gameMap[lastStepY - i, lastStepX + i] == player) count++;
+            }
+
+            // Check left bottom direction
+            if (lastStepX < Connect4Controller.BoardRow - lastStepY)
+                checkStepLeftBottom = lastStepX;
+            else
+                checkStepLeftBottom = Connect4Controller.BoardRow - lastStepY;
+
+            for (int i = 1; i < checkStepLeftBottom; i++)
+            {
+                if (gameMap[lastStepY + i, lastStepX - i] == player) count++;
+            }
+
+            if (count >= 4) return true;
+
 
             return false;
         }
 
         private static bool CheckDiagonal2(int player, int lastStepX, int lastStepY)
         {
-            // Check the second diagonal condition of last step
+            // This method is completed!!!
+
+            // Check the second diagonal (from left up to right bottom) condition of last step
+            int[,] gameMap = GameStatus.GameMap;
+            int count = 1;
+            int checkStepLeftUp;
+            int checkStepRightBottom;
+
+            // Check left up direction
+            if (lastStepX < lastStepY)
+                checkStepLeftUp = lastStepX;
+            else
+                checkStepLeftUp = lastStepY;
+
+            for (int i = 1; i <= checkStepLeftUp; i++)
+            {
+                if (gameMap[lastStepY - i, lastStepX - i] == player) count++;
+            }
+
+            // Check right bottom direction
+            if (Connect4Controller.BoardCol - lastStepX < Connect4Controller.BoardRow - lastStepY)
+                checkStepRightBottom = Connect4Controller.BoardCol - lastStepX;
+            else
+                checkStepRightBottom = Connect4Controller.BoardRow - lastStepY;
+
+            for (int i = 1; i < checkStepRightBottom; i++)
+            {
+                if (gameMap[lastStepY + i, lastStepX + i] == player) count++;
+            }
+
+            if (count >= 4) return true;
 
             return false;
         }
@@ -202,8 +341,10 @@ namespace Connect4_IreneChan
     {
         static void Main(string[] args)
         {
-            Screen.PrintGrids();
-            if(GameStatus.IsWin(1, 5, 5)) Console.WriteLine("Win!!!");
+            Connect4Controller.PlayConnect4();
+
+            //Screen.PrintGrids();
+            //if(GameStatus.IsWin(1, 3, 1)) Console.WriteLine("Win!!!");
         }
     }
 }
